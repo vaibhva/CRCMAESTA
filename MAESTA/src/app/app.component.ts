25c4a538104f3eed135eefa9b2c6f50dbe19f668
Neha Sharma <vaibhavkaushik19127@gmail.com>
2024-11-29 15:16:42 +0530

    Project intiate

--- MAESTA/src/app/app.component.ts
+++ MAESTA/src/app/app.component.ts
@@ -1,13 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+      RouterOutlet,
+      FormsModule,
+      CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'MAESTA';
+  dialig!:any;
+  isFormSubmit:boolean = false;
+  phonenumberDom!:any;
+  nameDom!:any;
+
+  ngAfterViewInit(){
+    const openModal = document.getElementById('openModal') as HTMLElement;
+    openModal.click();
+    this.nameDom = document.getElementById('name') as HTMLElement
+    this.phonenumberDom = document.getElementById('phonenumber') as HTMLElement
+  }
+  submitEntry(): void{
+    //this.nameDom.value
+//this.phonenumberDom.value
+    emailjs.init({
+      publicKey: "wjDP7u11ItqBjFwaw",
+    });
+    emailjs.send("service_qncwc6c","template_hul3pkb",{
+      from_name: this.nameDom.value,
+      from_phonenumber: this.phonenumberDom.value,
+      });
+    this.isFormSubmit = true;
+  }
+
 }
